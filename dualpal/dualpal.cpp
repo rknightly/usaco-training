2772c7fcd3295b00dd3021967f17a74727b87219
rknightly <reknightly@gmail.com>
2017-05-19 22:33:31 -0500

    Adding dualpal solution

--- dualpal/dualpal.cpp
+++ dualpal/dualpal.cpp
@@ -1,40 +1,61 @@
 /*
     PROB: dualpal
     LANG: C++11
-*/
+ */
 
-
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
 ifstream fin("dualpal.in");
 ofstream fout("dualpal.out");
 
+string numBaseN(int num, int base) {
+  string numInBase = "";
+  char digits[20] = "0123456789ABCDEFGHI";
+
+  while (num > 0) {
+    int remainder = num % base;
+    numInBase = digits[remainder] + numInBase;
+    num -= remainder;
+    num /= base;
+  }
+
+  return numInBase;
+}
+
+bool isPalindrome(string possiblePalindrome) {
+  string backwardsString = "";
+  for (int i = possiblePalindrome.length() - 1; i >= 0; i--) {
+    backwardsString += possiblePalindrome.at(i);
+  }
+  return possiblePalindrome == backwardsString;
+}
+
 bool isDualPal(int num) {
-    basePalCount = 0;
-    for(int base = 2; base <= 10; base++) {
-        if(isPalindrome(numBaseN(num, base))) {
-            basePalCount += 1;
-        }
+  int basePalCount = 0;
+  for (int base = 2; base <= 10; base++) {
+    if (isPalindrome(numBaseN(num, base))) {
+      basePalCount += 1;
     }
-    return basePalCount >= 2;
+  }
+  return basePalCount >= 2;
 }
 
 int main() {
-    int numCount;
-    int minNum;
-    fin >> numCount >> minNum;
+  int numCount;
+  int minNum;
+  fin >> numCount >> minNum;
 
-    int foundCount = 0;
+  int foundCount = 0;
 
-    for (int n = minNum + 1; foundCount < numCount; n++) {
-        if(isDualPal(n)) {
-            fout << n << endl;
-            foundCount ++;
-        }
+  for (int n = minNum + 1; foundCount < numCount; n++) {
+    if (isDualPal(n)) {
+      fout << n << endl;
+      foundCount++;
     }
+  }
 
-    return 0;
+  return 0;
 }
